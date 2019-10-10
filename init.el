55e6d530b1830b07bfa9bceb818bc8771693ff41
plwg <plwg@pm.me>
2019-10-10 22:23:27 +0800

    Added MELPA package management

--- init.el
+++ init.el
@@ -1,3 +1,10 @@
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+(package-initialize)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -5,6 +12,7 @@
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wheatgrass)))
  '(inhibit-startup-screen t)
+ '(package-selected-packages (quote (magit)))
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
@@ -14,3 +22,9 @@
  )
 (find-file "~/Dropbox/org/*.org" t)
 (find-file "~/.emacs.d/init.el")
+;; MELPA
+
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+
