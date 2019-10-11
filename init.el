7556f9df23426f330d5194f993db525445ef681f
plwg <plwg@pm.me>
2019-10-11 08:47:37 +0800

    Editor Apperance

--- init.el
+++ init.el
@@ -11,9 +11,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+ '(display-time-mode t)
+ '(fringe-mode 0 nil (fringe))
+ '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (magit)))
  '(package-selected-packages (quote (smart-mode-line magit)))
+ '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
