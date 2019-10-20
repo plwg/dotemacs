bc91c0115c8da53d4c2a1cf6b47c10bba47439e8
plwg <plwg@pm.me>
2019-10-20 20:40:11 +0800

    Check in Configuration

--- init.el
+++ init.el
@@ -10,15 +10,15 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes (quote (wheatgrass)))
+ '(custom-enabled-themes (quote (smart-mode-line-respectful)))
  '(custom-safe-themes
    (quote
-    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
+    ("c74e83f8aa4c78a121b52146eadb792c9facc5b1f02c917e3dbb454fca931223" "a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(display-time-mode t)
  '(fringe-mode 0 nil (fringe))
  '(global-display-line-numbers-mode t)
  '(inhibit-startup-screen t)
- '(package-selected-packages (quote (smart-mode-line magit)))
+ '(package-selected-packages (quote (evil dracula-theme smart-mode-line magit)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -50,4 +50,15 @@
 ;;smart mode line
 (setq sml/no-confirm-load-theme t)
 (sml/setup)
-(setq sml/theme 'dark)
+
+;;font
+
+(push '(font . "Inconsolata-14") default-frame-alist)
+
+;;python
+(setq python-shell-interpreter "/usr/local/bin/python3")
+
+;;theme
+
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
