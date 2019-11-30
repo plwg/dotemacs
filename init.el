6529b76f6200a1320c5714028fb3d7ac464a33bf
Paul Wang <plwg@pm.me>
2019-11-30 16:05:15 +0800

    Add Org Noter, Pdf Tools

--- init.el
+++ init.el
@@ -19,10 +19,12 @@
  '(display-time-mode t)
  '(doc-view-resolution 200)
  '(fringe-mode 0 nil (fringe))
- '(global-display-line-numbers-mode t)
+ '(global-display-line-numbers-mode nil)
  '(inhibit-startup-screen t)
  '(menu-bar-mode nil)
- '(package-selected-packages (quote (auto-complete evil smart-mode-line magit)))
+ '(package-selected-packages
+   (quote
+    (dracula-theme dr-racket-like-unicode org-noter pdf-tools auto-complete evil smart-mode-line magit)))
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -40,12 +42,11 @@
 (setq ido-everywhere t)
 (ido-mode 1)
 
-;; MELPA
+;; package
 
 (require 'package)
-(add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
-
+(add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 ;; magit
 
 (global-set-key (kbd "C-x g") 'magit-status)
@@ -60,85 +61,11 @@
 
 (ac-config-default)
 
-;; doc-view
-
-(with-eval-after-load "doc-view"
-  (easy-menu-define my-doc-view-menu doc-view-mode-map "Menu for Doc-View Mode."
-    '("DocView"
-      ["Switch to a different mode" doc-view-toggle-display :help "Switch to a different mode"]
-      ["Open Text" doc-view-open-text :help "Display the current doc's contents as text"]
-      "--"
-      ("Navigate Doc"
-       ["Goto Page ..." doc-view-goto-page :help "View the page given by PAGE"]
-       "--"
-       ["Scroll Down" doc-view-scroll-down-or-previous-page :help "Scroll page down ARG lines if possible, else goto previous page"]
-       ["Scroll Up" doc-view-scroll-up-or-next-page :help "Scroll page up ARG lines if possible, else goto next page"]
-       "--"
-       ["Next Line" doc-view-next-line-or-next-page :help "Scroll upward by ARG lines if possible, else goto next page"]
-       ["Previous Line" doc-view-previous-line-or-previous-page :help "Scroll downward by ARG lines if possible, else goto previous page"]
-       ("Customize"
-        ["Continuous Off"
-         (setq doc-view-continuous nil)
-         :help "Stay put in the current page, when moving past first/last line" :style radio :selected
-         (eq doc-view-continuous nil)]
-        ["Continuous On"
-         (setq doc-view-continuous t)
-         :help "Goto to the previous/next page, when moving past first/last line" :style radio :selected
-         (eq doc-view-continuous t)]
-        "---"
-        ["Save as Default"
-         (customize-save-variable 'doc-view-continuous doc-view-continuous)
-         t])
-       "--"
-       ["Next Page" doc-view-next-page :help "Browse ARG pages forward"]
-       ["Previous Page" doc-view-previous-page :help "Browse ARG pages backward"]
-       "--"
-       ["First Page" doc-view-first-page :help "View the first page"]
-       ["Last Page" doc-view-last-page :help "View the last page"])
-      "--"
-      ("Adjust Display"
-       ["Enlarge" doc-view-enlarge :help "Enlarge the document by FACTOR"]
-       ["Shrink" doc-view-shrink :help "Shrink the document"]
-       "--"
-       ["Fit Width To Window" doc-view-fit-width-to-window :help "Fit the image width to the window width"]
-       ["Fit Height To Window" doc-view-fit-height-to-window :help "Fit the image height to the window height"]
-       "--"
-       ["Fit Page To Window" doc-view-fit-page-to-window :help "Fit the image to the window"]
-       "--"
-       ["Set Slice From Bounding Box" doc-view-set-slice-from-bounding-box :help "Set the slice from the document's BoundingBox information"]
-       ["Set Slice Using Mouse" doc-view-set-slice-using-mouse :help "Set the slice of the images that should be displayed"]
-       ["Set Slice" doc-view-set-slice :help "Set the slice of the images that should be displayed"]
-       ["Reset Slice" doc-view-reset-slice :help "Reset the current slice"])
-      ("Search"
-       ["New Search ..."
-        (doc-view-search t)
-        :help "Jump to the next match or initiate a new search if NEW-QUERY is given"]
-       "--"
-       ["Search" doc-view-search :help "Jump to the next match or initiate a new search if NEW-QUERY is given"]
-       ["Backward" doc-view-search-backward :help "Call `doc-view-search' for backward search"]
-       "--"
-       ["Show Tooltip" doc-view-show-tooltip :help nil])
-      ("Maintain"
-       ["Reconvert Doc" doc-view-reconvert-doc :help "Reconvert the current document"]
-       "--"
-       ["Clear Cache" doc-view-clear-cache :help "Delete the whole cache (`doc-view-cache-directory')"]
-       ["Dired Cache" doc-view-dired-cache :help "Open `dired' in `doc-view-cache-directory'"]
-       "--"
-       ["Revert Buffer" doc-view-revert-buffer :help "Like `revert-buffer', but preserves the buffer's current modes"]
-       "--"
-       ["Kill Proc" doc-view-kill-proc :help "Kill the current converter process(es)"]
-       ["Kill Proc And Buffer" doc-view-kill-proc-and-buffer :help "Kill the current buffer"])
-      "--"
-      ["Customize"
-       (customize-group 'doc-view)]))
-  (easy-menu-define my-doc-view-minor-mode-menu doc-view-minor-mode-map "Menu for Doc-View Minor Mode."
-    '("DocView*"
-      ["Display in DocView Mode" doc-view-toggle-display :help "View"]
-      ["Exit DocView Mode" doc-view-minor-mode])))
-
 ;; org Mode
 
 (setq org-src-fontify-natively t)
+(require 'org-tempo)
+(setq org-directory "~/Dropbox/org")
 
 ;; evil
 
@@ -146,3 +73,16 @@
 (evil-mode 1)
 (eval-after-load 'evil-core
   '(evil-set-initial-state 'magit-popup-mode 'emacs))
+
+;; pdf-tools
+
+(pdf-tools-install)
+
+;; truncate line
+(set-default 'truncate-lines t)
+
+;; org noter
+
+(setq org-noter-always-create-frame nil)
+(setq org-noter-default-notes-file-names '("marginalia.org")
+      org-noter-notes-search-path '("~/Dropbox/org"))
